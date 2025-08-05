1f4a6b4caa7e8d529e2f4e09dd67e2d0205b82c0
Yegor10 <yehor.student@gmail.com>
2025-08-05 04:44:31 +0300

    Fixed user info command

--- cogs/user_info.py
+++ cogs/user_info.py
@@ -5,13 +5,9 @@ class UserInfo(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
-    @commands.slash_command(description="get info about user")
-    async def user_info(inter, user: disnake.Member):
-        is_bot = "Roar"
-        if user.bot == True:
-            is_bot == "Yes"
-        else:
-            is_bot == "No"
+    @commands.slash_command(description="Get info about user")
+    async def user_info(self, inter, user: disnake.Member):
+        is_bot = "Yes" if user.bot else "No"
         embed = disnake.Embed(
             title="All info about user",
             description=f"Shows all info about {user}",
@@ -24,8 +20,8 @@ class UserInfo(commands.Cog):
         embed.add_field(name="User ID: ", value=user.id, inline=True)
         embed.add_field(name="User's name in guild: ", value=inter.member.nick, inline=True)
         embed.add_field(name="Is user a bot: ", value=is_bot, inline=True)
-        embed.add_field(name="User joined discord at: ", value=user.created_at, inline=True)
-        embed.add_field(name=f"User joined {inter.guild.name} at: ", value=user.joined_at, inline=True)
+        embed.add_field(name="User joined discord at: ", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+        embed.add_field(name=f"User joined {inter.guild.name} at: ", value=user.joined_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
         await inter.response.send_message(embed=embed) 
 
 def setup(bot):
